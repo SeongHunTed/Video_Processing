efbb94478ebbc276ddc1c87e16ff270f3e3290a7
SeongHunTed <4047ksh@naver.com>
2022-05-03 01:35:03 +0900

    ch06 task_Done

--- ch06/11.task1.py
+++ ch06/11.task1.py
@@ -1,34 +1,31 @@
 import numpy as np
 import cv2
 
+title = "Blending"
+bar_name1 = "image1"
+bar_name2 = "image2"
+
 def onChange(value):
-    global image3, title, image1, image2, alpha
-    alpha = value/100
-    image3 = cv2.addWeighted(image1, alpha, image2, 1-alpha, 0)
-    dst1 = cv2.hconcat([image1, image3])
+
+    th[0] = cv2.getTrackbarPos(bar_name1, title)
+    alpha = th[0]/100
+    th[1] = cv2.getTrackbarPos(bar_name2, title)
+    beta = th[1]/100
+    dst = cv2.addWeighted(image1, alpha, image2, beta, 0)
+    dst1 = cv2.hconcat([image1, dst])
     dst2 = cv2.hconcat([dst1, image2])
     cv2.imshow(title, dst2)
 
-def onChange2(value):
-    global image3, title, image1, image2, alpha
-    alpha = value/100
-    image3 = cv2.addWeighted(image1, 1-alpha, image2, alpha, 0)
-    dst1 = cv2.hconcat([image1, image3])
-    dst2 = cv2.hconcat([dst1, image2])
-    cv2.imshow(title, dst2)
 
-alpha = 0
+th = [0, 0]
 image1 = cv2.imread("images/add1.jpg", cv2.IMREAD_GRAYSCALE)
 image2 = cv2.imread("images/add2.jpg", cv2.IMREAD_GRAYSCALE)
-image3 = cv2.addWeighted(image1, 1, image2, 1, 0)
-dst1 = cv2.hconcat([image1, image3])
+
+dst1 = cv2.hconcat([image1, image2])
 dst2 = cv2.hconcat([dst1, image2])
 
-title = "dst2"
-bar_name = "image1"
-bar_name2 = "image2"
-cv2.imshow(title, dst2)
-cv2.createTrackbar(bar_name, title, 1, 100, onChange)
-cv2.createTrackbar(bar_name2, title, 1, 100, onChange2)
 
+cv2.imshow(title, dst2)
+cv2.createTrackbar(bar_name1, title, th[0], 100, onChange)
+cv2.createTrackbar(bar_name2, title, th[1], 100, onChange)
 cv2.waitKey(0)
